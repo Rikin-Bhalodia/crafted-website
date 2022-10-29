1f3d3da28b315803af7bcb1aef3e0dcc8739b792
Rikin <rikinbhalodia9504@gmail.com>
2022-10-29 14:45:15 +0530

    fixed facebook pixel issues

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,32 @@ import Header from "../src/CommonComponent/Header/Header";
 import "../styles/globals.css";
 import "../styles/login-modal.css";
 import AuthProvider from "../src/auth/AuthContext";
-import { QueryClient, QueryClientProvider } from "react-query";
+// import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  const queryClient = new QueryClient();
+  const router = useRouter()
 
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1111798679308390')
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
   return (
     <>
       <AuthProvider>
         <Header />
-        <QueryClientProvider client={queryClient}>
+        {/* <QueryClientProvider client={queryClient}> */}
           <Component {...pageProps} />
-        </QueryClientProvider>
+        {/* </QueryClientProvider> */}
         <Footer />
       </AuthProvider>
     </>
